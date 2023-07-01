97e473b8970d8ccdd128a5654523a6482850597b
MrDragonneau <rzacharie07@gmail.com>
2023-07-01 20:32:04 +0200

    added jokes

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import discord
 from discord.ext import commands
 import os
+import requests
+import discord
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-bot = commands.Bot(command_prefix="!", description="bot de Zach")
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", description="bot de Zach", intents=intents)
 
 
 @bot.event
 async def on_ready():
     print("Bonjour je suis pret")
-
-
-# @bot.event
-# async def on_message(msg):
-#     if not msg.author.bot:
-#         await msg.channel.send("Bonjour!!!")
 
 
 @bot.event
@@ -24,36 +19,36 @@ async def on_message(msg):
         if msg.content == "Bonjour":
             await msg.channel.send("Hey")
 
-
-@bot.event
-async def on_message(msg):
-    if not msg.author.bot:
         if msg.content == "Au-revoir":
             await msg.channel.send("à bientôt")
-
-
 
     await bot.process_commands(msg)
 
 
-
-
 @bot.command()
 async def invitation(ctx):
-    # await ctx.send("https://discord.gg/cwaCNjf")
-    dm_chann = await ctx.author.create_dm()
-    await dm_chann.send("https://discord.gg/cwaCNjf")
+    dm_chan = await ctx.author.create_dm()
+    await dm_chan.send("https://discord.gg/6CVXCDvykM")
     await ctx.message.delete()
 
 
+@bot.command(aliases=["Vdm", "VDM"])
+async def vdm(ctx):
+    req = requests.get("https://blague.xyz/api/vdm/random")
+    req_json = req.json()
+    await ctx.channel.send(req_json["vdm"]["content"])
 
-    @bot.command()
-    async def invitation1(ctx):
-        # await ctx.send("https://discord.gg/NrHmGXT")
-        dm_chann = await ctx.author.create_dm()
-        await dm_chann.send("https://discord.gg/NrHmGXT")
-        await ctx.message.delete()
 
+@bot.command(aliases=["Blagues", "blagues", "Blague"])
+async def blague(ctx):
+    def SameChannel(msg):
+        return msg.channel == ctx.channel
+
+    req = requests.get("https://blague.xyz/api/joke/random")
+    req_json = req.json()
+    await ctx.channel.send(req_json["joke"]["question"])
+    await bot.wait_for("message", check=SameChannel)
+    await ctx.channel.send(req_json["joke"]["answer"])
 
 if __name__ == '__main__':
     bot.run(BOT_TOKEN)
